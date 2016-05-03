2b4aea9133e21b6881514e65d0a668377e813210
Anthony <AStabile.Design@gmail.com>
2016-05-03 01:30:44 -0500

    initial commit from template with minor alterations

---
+++ src/modules/modules.ctrl.js
@@ -0,0 +1,7 @@
+/* CONTROLLER FOR MODULES */
+app.controller('modCtrl', ['$scope',/* 'serv', */ function($scope/*, serv*/) {
+    mod = this;
+
+
+
+}]);//end modCtrl
---
+++ src/navigation/nav.routes.js
@@ -0,0 +1,23 @@
+app.config(['$routeProvider', '$locationProvider',
+  function($routeProvider, $locationProvider) {
+    $locationProvider.html5Mode(true);
+    $routeProvider
+      .when('/', {
+        redirectTo: "/home"
+      })
+      .when('/home', {
+        templateUrl: 'templates/home.html',
+        controller: 'homeCtrl as home'
+      })
+     .when('/modules', {
+        templateUrl: 'templates/modules.html',
+        controller: 'modCtrl as mod'
+     })
+     .when('/reflection', {
+        templateUrl: 'templates/reflection.html',
+        controller: 'reflectionCtrl as ref'
+     })
+      .otherwise({
+        redirectTo: "/home"
+      });
+}]);
